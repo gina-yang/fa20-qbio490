b6feae2a59eef2cebf92ba7889f8a5f3faa4a752
Gina Yang <ginayang@usc.edu>
2020-10-18 23:24:38 -0700

    clustering commands?

--- scvelo/crc_scvelo_run.py
+++ scvelo/crc_scvelo_run.py
@@ -7,14 +7,19 @@ crcdata = scv.read('kul01.loom', cache=True)
 crcdata.var_names_make_unique()
 crcdata
 
-# scv.pl.proportions(adata) # view proportion spliced to unspliced
+# scv.pl.proportions(adata) # View proportion spliced to unspliced (pie chart)
 
+# ??
 scv.pp.filter_and_normalize(crcdata, min_shared_counts=20, n_top_genes=2000)
+# 
 scv.pp.moments(crcdata, n_pcs=30, n_neighbors=30)
 
 scv.tl.velocity(crcdata)
 scv.tl.velocity_graph(crcdata)
 
-scv.tl.umap(crcdata) # set to umap
-scv.pl.velocity_embedding_stream(crcdata, basis='umap') # plot velocity 
-scv.pl.velocity(crcdata, ['CD3D',  'CD68', 'DCN', 'EPCAM', 'KIT', 'CD79A'], ncols=2) # view marker genes
+# Set to UMAP embedding
+scv.tl.umap(crcdata)
+# Plot velocity
+scv.pl.velocity_embedding_stream(crcdata, basis='umap')
+# Pass marker genes in list to plot expression/velocity
+scv.pl.velocity(crcdata, ['CD3D',  'CD68', 'DCN', 'EPCAM', 'KIT', 'CD79A'], ncols=2)
