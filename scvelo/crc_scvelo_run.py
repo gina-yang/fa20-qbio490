79906224e397d7fa27b1926459cdd1cdb5f8d541
Gina Yang <ginayang@usc.edu>
2020-10-19 18:47:10 -0700

    added louvain

--- scvelo/crc_scvelo_run.py
+++ scvelo/crc_scvelo_run.py
@@ -7,19 +7,26 @@ crcdata = scv.read('kul01.loom', cache=True)
 crcdata.var_names_make_unique()
 crcdata
 
-# scv.pl.proportions(adata) # View proportion spliced to unspliced (pie chart)
+# scv.pl.proportions(adata) # View proportion spliced/unspliced counts (in pie chart)
 
-# ??
+# Preprocessing
+# Filter genes based on number of counts; extract highly variable genes; normalize each 
+# cell by total counts over all genes; logarithmize the data matrix
+	# min_shared_counts: minimum number of counts (both spliced and unspliced) required for a gene
+	# n_top_genes: number of highly variable genes to okeep
 scv.pp.filter_and_normalize(crcdata, min_shared_counts=20, n_top_genes=2000)
-# 
+# Computes moments for velocity estimation with pca and computing neighbors
 scv.pp.moments(crcdata, n_pcs=30, n_neighbors=30)
 
 scv.tl.velocity(crcdata)
 scv.tl.velocity_graph(crcdata)
 
+
+# scv.tl.louvain(crcdata) # resolution default=1
+
 # Set to UMAP embedding
 scv.tl.umap(crcdata)
-# Plot velocity
+# Plot velocity as streamlines
 scv.pl.velocity_embedding_stream(crcdata, basis='umap')
 # Pass marker genes in list to plot expression/velocity
 scv.pl.velocity(crcdata, ['CD3D',  'CD68', 'DCN', 'EPCAM', 'KIT', 'CD79A'], ncols=2)
