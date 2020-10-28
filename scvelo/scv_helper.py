e63ab66a8c71153db7bef578b3c08df77a0d0460
Gina Yang <ginayang@usc.edu>
2020-10-28 10:56:30 -0700

    added velocity plot of all kul19 cells

--- scvelo/scv_helper.py
+++ scvelo/scv_helper.py
@@ -1,6 +1,6 @@
 import scvelo as scv
 
-# Perform setup, data loading, and preprocessing
+# Perform setup and data loading
 # Returns object that stores data matrix with observations and variables
 def setup(loomfile):
 	scv.settings.verbocity = 3
@@ -9,18 +9,23 @@ def setup(loomfile):
 
 	crcdata = scv.read(loomfile, cache=True)
 	crcdata.var_names_make_unique()
+	return(crcdata)
 
-	# Preprocessing
+
+# crcdata # shows dimensions: n_obs x n_vars (n_obs=number of cells, n_vars=cell info)
+# adata = adata1.concatenate(adata2) # to concatenate two data objects (adata = adata1 + adata2)
+
+
+# Preprocessing
 	# Filter genes based on number of counts; extract highly variable genes; normalize each 
 	# cell by total counts over all genes; logarithmize the data matrix
 		# min_shared_counts: minimum number of counts (both spliced and unspliced) required for a gene
 		# n_top_genes: number of highly variable genes to keep
-	scv.pp.filter_and_normalize(crcdata, min_shared_counts=20, n_top_genes=2000)
+def preproc(data_obj):
+	scv.pp.filter_and_normalize(data_obj, min_shared_counts=20, n_top_genes=2000)
 	# Computes moments for velocity estimation with pca and computing neighbors
-	scv.pp.moments(crcdata, n_pcs=30, n_neighbors=30)
-	return(crcdata)
-
-# crcdata # shows dimensions: n_obs x n_vars (n_obs=number of cells, n_vars=cell info)
+	scv.pp.moments(data_obj, n_pcs=30, n_neighbors=30)
+	return(data_obj)
 
 # Estimates RNA velocity and projects and visualizes them
 def plot_velocity(data_obj, louvain_res):
@@ -31,8 +36,8 @@ def plot_velocity(data_obj, louvain_res):
 	scv.tl.umap(data_obj)
 	# Plot velocity as streamlines
 	scv.pl.velocity_embedding_stream(data_obj, basis='umap')
-	# Pass marker genes in list to plot expression/velocity
 
+# Pass marker genes in list to plot phase portraits
 # marker_genes = ['CD3D', 'CD68', 'DCN', 'EPCAM', 'KIT', 'CD79A']
 
 # Plot phase portraits of marker genes
